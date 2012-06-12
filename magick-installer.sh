815e0f399e99180ca2b0412f0776ef56f2c7ce32
Hai <hai.lin.cs@gmail.com>
2012-06-12 00:52:47 -0300

    Update master

--- magick-installer.sh
+++ magick-installer.sh
@@ -36,7 +36,7 @@ cd ../..
 
 tar xzvf freetype-2.4.3.tar.gz
 cd freetype-2.4.3
-./configure --prefix=/usr/local --disable-openmp
+./configure --prefix=/usr/local 
 make clean
 make
 sudo make install
